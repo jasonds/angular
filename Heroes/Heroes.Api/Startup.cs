f15b174119fd619cae56491f57514471d76f79d4
Jason Sears <jasondouglassears@gmail.com>
2019-02-24 18:30:53 -0600

    Upgraded to 2.2.

--- Heroes/Heroes.Api/Startup.cs
+++ Heroes/Heroes.Api/Startup.cs
@@ -18,7 +18,7 @@ namespace Heroes.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddCors();
         }
@@ -32,6 +32,7 @@ namespace Heroes.Api
             }
             else
             {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
